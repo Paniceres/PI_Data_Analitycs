de42275f2e304f2a31ebab1d99477f0c90f907bd
Lucio Paniceres <paniceres73@gmail.com>
2023-11-08 00:17:03 -0300

    fix(deploy): route load data error

--- deploy/main.py
+++ deploy/main.py
@@ -27,16 +27,23 @@ df_kpi = data_load.dataframes['kpi']
 # Filtrar provincias con tasa de Penetracion > 60% en 2022
 provincias_criterio = calcular_provincias_criterio(df_kpi)
 
-# Cargar el logo desde el archivo
-logo_path = './static/ENACOM_logo.png'
-logo = Image.open(logo_path)
+# -------------- logo
+# Obtener la ruta al directorio actual (donde se encuentra main.py)
+current_directory = os.path.dirname(os.path.abspath(__file__))
 
-# Mostrar el logo en Streamlit
-st.sidebar.image(logo, width=300)
+# Construir la ruta a la imagen usando os.path.join
+static_directory = os.path.join(current_directory, 'static')
+logo_path = os.path.join(static_directory, 'ENACOM_logo.png')
 
+# Abrir la imagen
+try:
+    logo = Image.open(logo_path)
+except FileNotFoundError:
+    print(f"Error: No se pudo encontrar la imagen en la ruta: {logo_path}")
+# -------------- logo
 
 # Interactividad con Streamlit
-st.title('Proyecto: Análisis de ENACOM y su calidad y penetración de servicio')
+st.title('Proyecto: Análisis de ENACOM: Validad y Penetración de servicio en torno a tecnologías.')
 
 pagina_seleccionada = st.sidebar.radio('Selecciona una página:',
                                        ['Contexto', 'Criterio', 'Penetración', 'Tecnología'])
